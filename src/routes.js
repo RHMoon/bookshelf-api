c6f0041f6ce3b51c71771b6c096b5a4edbb4ddce
Naufal Muhammad Farras <nmfarras@gmail.com>
2022-02-12 00:27:04 +0700

    init

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  editBookByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: () => {},
+  },
+];
+
+module.exports = routes;
