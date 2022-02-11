bd79cd804b7c2dbbef29d8cc010d58da55ace33a
Naufal Muhammad Farras <nmfarras@gmail.com>
2022-02-12 00:33:03 +0700

    resolve bug. data will not update if there is an error in body request.

--- src/handler.js
+++ src/handler.js
@@ -22,6 +22,24 @@ const addBookHandler = (request, h) => {
     finished = true;
   }
 
+  if (name === undefined) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message:
+        'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
   const newBook = {
     id,
     name,
@@ -41,43 +59,24 @@ const addBookHandler = (request, h) => {
 
   const isSuccess = books.filter((book) => book.id === id).length > 0;
 
-  // if (name === undefined) {
-  //   const response = h.response({
-  //     status: 'fail',
-  //     message: 'Gagal menambahkan buku. Mohon isi nama buku',
-  //   });
-  //   response.code(400);
-  //   return response;
-  // }
-  // if (readPage > pageCount) {
-  //   const response = h.response({
-  //     status: 'fail',
-  //     message:
-  //       'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
-  //   });
-  //   response.code(400);
-  //   return response;
-  // }
-  try {
-    if (isSuccess) {
-      const response = h.response({
-        status: 'success',
-        message: 'Buku berhasil ditambahkan',
-        data: {
-          bookId: id,
-        },
-      });
-      response.code(201);
-      return response;
-    }
-  } catch (error) {
+  if (isSuccess) {
     const response = h.response({
-      status: 'fail',
-      message: 'Buku gagal ditambahkan',
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
     });
-    response.code(500);
+    response.code(201);
     return response;
   }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
 };
 
 const getAllBooksHandler = () => {
